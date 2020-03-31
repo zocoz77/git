7dc2fac1bd76f22e842bb31cca23ffa2992497aa
Carl Mahan <mahanc@hotmail.com>
2020-03-31 15:09:08 -0400

    fe

---
+++ src/git/Git.java
@@ -0,0 +1,19 @@
+/*
+ * CMSC 350 Professor Elizes
+ */
+package git;
+
+/**
+ *
+ * @author mahan
+ */
+public class Git {
+
+    /**
+     * @param args the commffand line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
