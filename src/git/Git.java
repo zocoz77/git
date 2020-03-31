7299ddf7a1b2e5f2fcaa3509191f4a496f47f0ad
Carl Mahan <mahanc@hotmail.com>
2020-03-31 15:13:03 -0400

    initial commit

--- src/git/Git.java
+++ src/git/Git.java
@@ -13,7 +13,7 @@ public class Git {
      * @param args the commffand line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello world");
     }
     
 }
